afc41bea8c7c56ec67b656d4a33354574557eb4c
Priyanshu078 <priyanshupaliwal078@gmail.com>
2023-09-07 10:05:36 +0530

    developed api's

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
