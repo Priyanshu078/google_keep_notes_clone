e0d781ff72a977bcbc2c8489afd9fe4534409ff2
Priyanshu078 <priyanshupaliwal078@gmail.com>
2023-09-13 21:43:07 +0530

    added bloc

---
+++ frontend/notes_app/lib/notes_bloc/notes_bloc.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'notes_event.dart';
+import 'notes_states.dart';
+
+class NotesBloc extends Bloc<NotesEvent, NotesStates> {
+  NotesBloc() : super(InitialNotesState());
+}
---
+++ frontend/notes_app/lib/notes_bloc/notes_states.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+
+class NotesStates extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class InitialNotesState extends NotesStates {}
