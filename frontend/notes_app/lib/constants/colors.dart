f2e8de5e4178919b92e5cff15458d3a731fabeb6
Priyanshu078 <priyanshupaliwal078@gmail.com>
2023-09-20 23:16:39 +0530

    updated colors of notes

--- frontend/notes_app/lib/constants/colors.dart
+++ frontend/notes_app/lib/constants/colors.dart
@@ -1,26 +1,29 @@
 import 'dart:ui';
 
-Color white = const Color(0xffFFFFFF);
-Color coral = const Color(0xffF88379);
-Color peach = const Color(0xffFFB07C);
-Color sand = const Color(0xffF0DC88);
-Color mint = const Color(0xffCBFFFA);
-Color storm = const Color(0xff507B9C);
-Color dusk = const Color(0xffA667AB);
-Color palepink = const Color(0xffCC7D9A);
-Color cherryBlossom = const Color(0xffF7CEE0);
+// Color white = const Color(0xffFFFFFF);
+// Color coral = const Color(0xffF88379);
+// Color peach = const Color(0xffFFB07C);
+// Color sand = const Color(0xffF0DC88);
+// Color mint = const Color(0xffCBFFFA);
+// Color storm = const Color(0xff507B9C);
+// Color dusk = const Color(0xffA667AB);
+// Color palepink = const Color(0xffCC7D9A);
+// Color cherryBlossom = const Color(0xffF7CEE0);
 Color textFieldBackgoundColor = const Color(0xfff4f7fc);
 Color bottomBannerColor = const Color(0xfff2f5fc);
 Color fabColor = const Color(0xffeff4fa);
 
 const List<Color> colors = [
   Color(0xffFFFFFF),
-  Color(0xffF88379),
-  Color(0xffFFB07C),
-  Color(0xffF0DC88),
-  Color(0xffCBFFFA),
-  Color(0xffA667AB),
-  Color(0xff507B9C),
-  Color(0xffCC7D9A),
-  Color(0xffF7CEE0),
+  Color(0xfffaafa9),
+  Color(0xfff29f75),
+  Color(0xfffff8b9),
+  Color(0xffcfdfc2),
+  Color(0xffa6c9c2),
+  Color(0xffd3e4ec),
+  Color(0xffa1bbc8),
+  Color(0xffd3bedb),
+  Color(0xfff5e2dc),
+  Color(0xffe9e3d3),
+  Color(0xffefeff1),
 ];
